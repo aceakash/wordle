faa2177a0daf3e76d2bf1585981e233d8bbe76ed
Akash Kurdekar <akashkurdekar@gmail.com>
2022-02-03 18:03:19 +0000

    hack done!

--- cmd/wordle/main.go
+++ cmd/wordle/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/fatih/color"
 	"io"
 	"math/rand"
 	"os"
@@ -16,24 +17,30 @@ func main() {
 		panic(err)
 	}
 	ui := NewUI(os.Stdin, os.Stdout, os.Stderr)
-	err = ui.RenderGameState(game)
-	if err != nil {
-		panic(err)
-	}
-	guess, err := ui.GetGuessFromUser()
-	//fmt.Println("guess", guess)
-	if err != nil {
-		panic(err)
-	}
-	//fmt.Println("===", guess, game.Secret(), guess == game.Secret(), "===")
-	if guess == game.Secret() {
-		//fmt.Println("in")
-		err := ui.GameSolved()
+
+	for i := 0; i < 6; i++ {
+		err = ui.RenderGameState(game)
 		if err != nil {
 			panic(err)
 		}
-		return
+		guess, err := ui.GetGuessFromUser()
+		//fmt.Println("guess", guess)
+		if err != nil {
+			panic(err)
+		}
+		//fmt.Println("===", guess, game.Secret(), guess == game.Secret(), "===")
+		if guess == game.Secret() {
+			//fmt.Println("in")
+			err := ui.GameSolved()
+			if err != nil {
+				panic(err)
+			}
+			break
+		}
+		game.RegisterGuess(guess)
 	}
+	ui.RenderGameState(game)
+	ui.NotSolved(game.Secret())
 }
 
 type UI struct {
@@ -42,14 +49,31 @@ type UI struct {
 	err io.Writer
 }
 
-func (ui UI) RenderGameState(game Game) error {
+func (ui UI) RenderGameState(game *Game) error {
 	_, err := fmt.Fprintln(ui.out, "==========")
 	if err != nil {
 		return err
 	}
-	_, err = fmt.Fprintf(ui.out, "The secret is %s\n", game.Secret())
-	if err != nil {
-		return err
+	//_, err = fmt.Fprintf(ui.out, "The secret is %s\n", game.Secret())
+	//if err != nil {
+	//	return err
+	//}
+	green := color.New(color.BgGreen)
+	yellow := color.New(color.BgYellow)
+	black := color.New(color.BgHiBlack)
+	for _, c := range game.Clues() {
+		for _, r := range c {
+			if r.Type == SpotOn {
+				green.Fprintf(ui.out, "%c ", r.Letter)
+				continue
+			}
+			if r.Type == WrongPlace {
+				yellow.Fprintf(ui.out, "%c ", r.Letter)
+				continue
+			}
+			black.Fprintf(ui.out, "%c ", r.Letter)
+		}
+		fmt.Fprintln(ui.out, "")
 	}
 	return nil
 }
@@ -75,25 +99,83 @@ func (ui UI) GameSolved() error {
 	return nil
 }
 
+func (ui UI) NotSolved(secret string) {
+	fmt.Fprintf(ui.out, "Unlucky, the word was %s\n", secret)
+}
+
 func NewUI(in io.Reader, out, err io.Writer) UI {
 	return UI{out, in, err}
 }
 
 type Game struct {
-	secret string
+	secret  string
+	guesses []string
+	clues   []Clue
 }
 
-func (g Game) Secret() string {
+func (g *Game) Secret() string {
 	return g.secret
 }
 
-func NewGame() (Game, error) {
+func (g *Game) RegisterGuess(guess string) {
+	g.guesses = append(g.guesses, guess)
+	clue := g.generateClue(guess)
+	g.clues = append(g.clues, clue)
+}
+
+type ClueLetterType string
+
+const (
+	SpotOn     ClueLetterType = "SpotOn"
+	WrongPlace ClueLetterType = "WrongPlace"
+	Wrong      ClueLetterType = "Wrong"
+)
+
+type ClueLetter struct {
+	Type   ClueLetterType
+	Letter rune
+}
+
+type Clue []ClueLetter
+
+func (g *Game) Clues() []Clue {
+	return g.clues
+}
+
+func (g *Game) generateClue(guess string) Clue {
+	clue := []ClueLetter{}
+	for i, _ := range guess {
+		if g.secret[i] == guess[i] {
+			clue = append(clue, ClueLetter{
+				Type:   SpotOn,
+				Letter: rune(guess[i]),
+			})
+			continue
+		}
+		if strings.ContainsRune(g.secret, rune(guess[i])) {
+			clue = append(clue, ClueLetter{
+				Type:   WrongPlace,
+				Letter: rune(guess[i]),
+			})
+			continue
+		}
+		clue = append(clue, ClueLetter{
+			Type:   Wrong,
+			Letter: rune(guess[i]),
+		})
+	}
+	return clue
+}
+
+func NewGame() (*Game, error) {
 	secret, err := pickSecret()
 	if err != nil {
-		return Game{}, err
+		return &Game{}, err
 	}
-	game := Game{
+	game := &Game{
 		secret,
+		[]string{},
+		[]Clue{},
 	}
 	return game, nil
 }
