15dd01684b10e25f872c204f0f1dcbb31a0c774e
Akash Kurdekar <akashkurdekar@gmail.com>
2022-02-12 12:57:54 +0000

    correct letters are marked green : more testing

--- internal/game_test.go
+++ internal/game_test.go
@@ -13,59 +13,146 @@ func TestNewGame(t *testing.T) {
 	})
 }
 
-func TestGame_WhenOnlyFirstLetterIsCorrect(t *testing.T) {
-	g, _ := i.NewGame("folds")
-	res := g.Guess("freak")
+func TestGame_CorrectLettersAreMarked(t *testing.T) {
+	t.Run("first letter only", func(t *testing.T) {
+		g, _ := i.NewGame("folds")
+		res := g.Guess("freak")
 
-	expectedClues := [5]i.Clue{i.Clue{
-		Letter: 'F',
-		Result: i.Correct,
-	},
-		i.Clue{
+		expectedClues := [5]i.Clue{i.Clue{
+			Letter: 'F',
+			Result: i.Correct,
+		},
+			i.Clue{
+				Letter: 'R',
+				Result: i.Wrong,
+			},
+			i.Clue{
+				Letter: 'E',
+				Result: i.Wrong,
+			},
+			i.Clue{
+				Letter: 'A',
+				Result: i.Wrong,
+			},
+			i.Clue{
+				Letter: 'K',
+				Result: i.Wrong,
+			},
+		}
+		assert.Equal(t, i.GuessResult{Solved: false, Clues: expectedClues}, res)
+	})
+
+	t.Run("third letter only", func(t *testing.T) {
+		g, _ := i.NewGame("folds")
+		res := g.Guess("ruler")
+
+		expectedClues := [5]i.Clue{i.Clue{
 			Letter: 'R',
 			Result: i.Wrong,
 		},
-		i.Clue{
-			Letter: 'E',
+			i.Clue{
+				Letter: 'U',
+				Result: i.Wrong,
+			},
+			i.Clue{
+				Letter: 'L',
+				Result: i.Correct,
+			},
+			i.Clue{
+				Letter: 'E',
+				Result: i.Wrong,
+			},
+			i.Clue{
+				Letter: 'R',
+				Result: i.Wrong,
+			},
+		}
+		assert.Equal(t, i.GuessResult{Solved: false, Clues: expectedClues}, res)
+	})
+
+	t.Run("fifth letter only", func(t *testing.T) {
+		g, _ := i.NewGame("folds")
+		res := g.Guess("runes")
+
+		expectedClues := [5]i.Clue{i.Clue{
+			Letter: 'R',
 			Result: i.Wrong,
 		},
-		i.Clue{
-			Letter: 'A',
-			Result: i.Wrong,
-		},
-		i.Clue{
-			Letter: 'K',
-			Result: i.Wrong,
-		},
-	}
-	assert.Equal(t, i.GuessResult{Solved: false, Clues: expectedClues}, res)
-}
+			i.Clue{
+				Letter: 'U',
+				Result: i.Wrong,
+			},
+			i.Clue{
+				Letter: 'N',
+				Result: i.Wrong,
+			},
+			i.Clue{
+				Letter: 'E',
+				Result: i.Wrong,
+			},
+			i.Clue{
+				Letter: 'S',
+				Result: i.Correct,
+			},
+		}
+		assert.Equal(t, i.GuessResult{Solved: false, Clues: expectedClues}, res)
+	})
 
-func Test_WhenGuessIsCorrectOnFirstTry(t *testing.T) {
-	g, _ := i.NewGame("folds")
-	res := g.Guess("folds")
+	t.Run("first and fifth letters only", func(t *testing.T) {
+		g, _ := i.NewGame("folds")
+		res := g.Guess("frees")
 
-	expectedClues := [5]i.Clue{i.Clue{
-		Letter: 'F',
-		Result: i.Correct,
-	},
-		i.Clue{
-			Letter: 'O',
+		expectedClues := [5]i.Clue{i.Clue{
+			Letter: 'F',
 			Result: i.Correct,
 		},
-		i.Clue{
-			Letter: 'L',
-			Result: i.Correct,
-		},
-		i.Clue{
-			Letter: 'D',
-			Result: i.Correct,
-		},
-		i.Clue{
-			Letter: 'S',
-			Result: i.Correct,
-		},
-	}
-	assert.Equal(t, i.GuessResult{Solved: false, Clues: expectedClues}, res)
+			i.Clue{
+				Letter: 'R',
+				Result: i.Wrong,
+			},
+			i.Clue{
+				Letter: 'E',
+				Result: i.Wrong,
+			},
+			i.Clue{
+				Letter: 'E',
+				Result: i.Wrong,
+			},
+			i.Clue{
+				Letter: 'S',
+				Result: i.Correct,
+			},
+		}
+		assert.Equal(t, i.GuessResult{Solved: false, Clues: expectedClues}, res)
 
+	})
+
+	t.Run("all correct", func(t *testing.T) {
+		g, _ := i.NewGame("folds")
+		res := g.Guess("folds")
+
+		expectedClues := [5]i.Clue{i.Clue{
+			Letter: 'F',
+			Result: i.Correct,
+		},
+			i.Clue{
+				Letter: 'O',
+				Result: i.Correct,
+			},
+			i.Clue{
+				Letter: 'L',
+				Result: i.Correct,
+			},
+			i.Clue{
+				Letter: 'D',
+				Result: i.Correct,
+			},
+			i.Clue{
+				Letter: 'S',
+				Result: i.Correct,
+			},
+		}
+		assert.Equal(t, i.GuessResult{Solved: false, Clues: expectedClues}, res)
+
+	})
 }
